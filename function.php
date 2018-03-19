3ac05d32cae50d0fd58055237071fde27a4ae1dd
Miroslav P <miroslav.petkevic@gmail.com>
2018-03-19 12:11:32 +0200

    Modified for creating full sentences

--- function.php
+++ function.php
@@ -8,9 +8,16 @@ function show()
     $wordsArray = splitter($file);
     $number = $_GET['number'];
     for($i = 1; $i <= $number; $i++) {
+        $random = rand(6,10);
         $sent = $wordsArray[rand(0, count($wordsArray)-1)];
         $word = $sent[rand(0, count($sent) -1)];
-        echo $i . ": " . $word. " ";
+        if ($i == 1) {
+            echo ucfirst($word);
+        } elseif ($i % $random == 0) {
+            echo ". " . ucfirst($word);
+        } else {
+            echo " " .$word;
+        }
     }
 }
 
@@ -19,6 +26,7 @@ function splitter($file)
 {
     $text = file_get_contents($file);
     $text = str_replace(". ", "=|=", $text);
+    $text = strtolower($text);
     $sentence = explode("=|=", trim($text));
     array_pop($sentence);
     $words = [];
